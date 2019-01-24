fb8a1f27c4b3c37104b355f0cb493bad3498f969
Ezk24 <Ezk24@DESKTOP-O4DUUU6>
2019-01-24 12:16:46 +0100

    3.Clase Aula

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/Aula.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.reservasaulas.modelo.dominio;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Ezk24
+ */
+public class Aula {
+    
+    private String nombre;
+
+    public Aula(String nombre) {
+        
+        setNombre(nombre);
+    }
+    
+    public Aula(Aula aula){
+    
+    if (aula==null){
+        
+        throw new IllegalArgumentException("No se puede copiar un aula nula.");
+    }
+    if (aula.getNombre().equals("")){
+        
+        throw new IllegalArgumentException("No se puede copiar un aula vacia.");
+        
+    }else{
+    
+        setNombre(aula.getNombre());
+        
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    private void setNombre(String nombre){
+        
+        if(nombre==null){
+        
+            throw new IllegalArgumentException("El nombre del aula no puede ser nulo.");
+        }
+        if(nombre.equals("")){
+            
+            throw new IllegalArgumentException("El nombre del aula no puede estar vacio.");
+            
+        }else{
+            
+            this.nombre = nombre;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.nombre);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Aula other = (Aula) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return  "[nombre=" + nombre + ']';
+    }
+    
+    
+    
+    
+    
+    
+    
+}
