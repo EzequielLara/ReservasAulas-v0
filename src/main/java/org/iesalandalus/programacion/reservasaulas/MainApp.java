966ed69e9ff0cb97dea5bbf1b9456f3296728c23
Ezk24 <Ezk24@DESKTOP-O4DUUU6>
2019-02-11 22:31:33 +0100

    clase Main

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -1,9 +1,10 @@
 package org.iesalandalus.programacion.reservasaulas;
-
+import org.iesalandalus.programacion.reservasaulas.vista.IUTextual;
 public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("Programa para la gestión de reservas de espacios del IES Al-Ándalus");
+        IUTextual programa = new IUTextual();
+        programa.comenzar();
 	}
-
 }
